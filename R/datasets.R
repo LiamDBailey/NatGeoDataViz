ffe1ec01ccad4e39b7c292e73b8f1a01a89bb383
Liam D. Bailey <liam.bailey@liamdbailey.com>
2019-06-01 15:00:45 +0200

    Initial commit
    Extract data from Geyer et al. 2017 for use in data viz. Write functions for extraction.

---
+++ R/datasets.R
@@ -0,0 +1,24 @@
+#' Names of plastic resins and sector types
+#'
+#' Standard names for each plastic resin and sector type
+#' used in Geyer et al. 2017.
+#' @format A list with 2 items.
+#' \describe{
+#'   \item{plastics}{Standard names of plastic resin types.}
+#'   \item{sectors}{Standard names of industry sectors.}
+#'   }
+#'@name standard_names
+NULL
+
+#' Data extracted from Geyer et al. 2017
+#'
+#' Useful data extracted from Geyer et al. 2017.
+#' @format A list with 4 items.
+#' \describe{
+#'   \item{total_plastic}{A data frame with total global plastic production from 1950 - 2015.}
+#'   \item{sectors_waste}{A data frame with 2015 plastic production and plastic waste generation per industry sector.}
+#'   \item{plastics_waste}{A data frame with 2015 plastic production and plastic waste generation per plastic resin type.}
+#'   \item{sectors_plastic}{Proportion of plastic resin types used in each industry sector between 2002 - 2014.}
+#'   }
+#'@name standard_names
+NULL
